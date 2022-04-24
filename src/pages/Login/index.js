c48933749c92ebcbc933f325b9f5a99df5a1d55b
wendel92 <wendel_baterista@hotmail.com>
2022-04-23 23:27:06 -0300

    Config page login index.js

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,61 +1,56 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import Header from '../../componentes/Header'; //IMPORTAÇÃO DO COMPONENTE HEADER PARA A TELA DE LOGIN
-import { LayoutComponents } from '../../componentes/LayoutComponents';
-import axios from 'axios';
-import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import Header from '../../componentes/Header' //IMPORTAÇÃO DO COMPONENTE HEADER PARA A TELA DE LOGIN
+import { LayoutComponents } from '../../componentes/LayoutComponents'
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
 
-
-// TELA DE LOGIN 
+// TELA DE LOGIN
 export default function Login() {
+  const navigate = useNavigate()
 
-const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
 
-const {
-      
-  register,
-  handleSubmit,
-    
-    
-    
     formState: { errors },
-  } = useForm();
+  } = useForm()
 
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  
-  const onSubmit = data => {
-    alert(data.email + '\n' + data.senha);
-    
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  const onSubmit = (data) => {
+    alert(data.email + '\n' + data.senha)
+
     const api = axios.create({
-      baseURL: "http://localhost:8000",
-    });
-    
-    api.post('/login', 
-    {
-      email: data.email,
-      password: data.senha
+      baseURL: 'http://localhost:8000',
     })
 
-    .then(function(data){
-      console.log(data);
+    api
+      .post('/login', {
+        email: data.email,
+        password: data.senha,
+      })
 
-    }).catch(function(e){
-      console.log(e)
-    });
-   
+      .then(function (data) {
+        console.log(data)
+      })
+      .catch(function (e) {
+        console.log(e)
+      })
+  }
 
-  };
-  
-    return (
+  return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)} className="login-form">
       <Header />
       <LayoutComponents>
-        <p><span className="login-form-title">Login</span></p>
+        <p>
+          <span className="login-form-title">Login</span>
+        </p>
         <span className="login-form-title"></span>
         {/* register your input into the hook by invoking the "register" function */}
-        
+
         {/* Campo email */}
         <div className="wrap-input">
           <input
@@ -63,26 +58,28 @@ const {
             className={email !== '' ? 'has-val input' : 'input'}
             type="text"
             value={email}
-            onChange={e => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <span className="focus-input" data-placeholder="Email"></span>
         </div>
-         
-         {/* Campo Senha */}
+
+        {/* Campo Senha */}
         <div className="wrap-input">
           <input
             {...register('senha')}
             className={senha !== '' ? 'has-val input' : 'input'}
             type="password"
             value={senha}
-            onChange={e => setSenha(e.target.value)}
+            onChange={(e) => setSenha(e.target.value)}
           />
           <span className="focus-input" data-placeholder="senha"></span>
         </div>
-         
+
         {/* Botão de login */}
-        <button className="area-botao" onClick={() => navigate('/')}>Login</button>
-        
+        <button className="area-botao" onClick={() => navigate('/')}>
+          Login
+        </button>
+         {/* texto da parte inferior */}
         <div className="text-center">
           <span className="txt1">Não possui conta? </span>
           <Link className="txt2" to="/register">
@@ -91,5 +88,5 @@ const {
         </div>
       </LayoutComponents>
     </form>
-  );
+  )
 }
