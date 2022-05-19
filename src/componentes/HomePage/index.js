6b77f8d7e2a2bfa7bd66e6d16e7696d0a61dfd09
ViniciusM11 <noturno793@gmail.com>
2022-05-18 23:28:33 -0300

    exclusao_Import_desncessario

--- src/componentes/HomePage/index.js
+++ src/componentes/HomePage/index.js
@@ -1,7 +1,6 @@
 import './style.css'
 import React from 'react'
 import Card from '../../componentes/Card'
-import Footer from './../Footer/index';
 
 export default function HomePage() {
   return (
