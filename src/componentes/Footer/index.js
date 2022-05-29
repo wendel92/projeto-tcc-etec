adb6d8bf009cd1fea2268589218ff55e81c7f7ea
ViniciusM11 <noturno793@gmail.com>
2022-05-29 17:25:25 -0300

    modificacao do rodape

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -12,55 +12,59 @@ import { faBarcode } from "@fortawesome/free-solid-svg-icons";
 export default function Footer() {
   return (
     <footer>
-      <div className="container">
-        <div className="row">
-          {/* Coluna 1 */}
-          <div className="col">
-            <h5>Endereço</h5>
-            <ul className="list-unstyled">
-              <li>Av. Cacilda, 137</li>
-              <li>06322-000</li>
-              <li>Carapicuiba, SP</li>
-            </ul>
-          </div>
-          {/* Coluna 2 */}
-          <div className="col">
-            <h5>Contatos</h5>
-            <ul className="list-unstyled">
-              <li>
-                <a href="(11)3659-8524">
-                  <BsTelephone />(11) 3659-8524
-                </a>
-              </li>
-              <li>
-                <a href="asiluminadas@gmail.com">
-                  <MdOutlineAlternateEmail /> asiluminadas@gmail.com
-                </a>
-              </li>
-            </ul>
-          </div>
-          {/* Coluna 3 */}
-          <div className="col">
+      <div className="cont-foot">
+        <div className="cont-foot-2">
+          <div className="row">
+            {/* Coluna 1 */}
+            <div className="col">
+              <h5>Endereço</h5>
+              <ul className="list-unstyled">
+                <li>Av. Cacilda, 137</li>
+                <li>06322-000</li>
+                <li>Carapicuiba, SP</li>
+              </ul>
+            </div>
+            {/* Coluna 2 */}
+            <div className="col">
+              <h5>Contatos</h5>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="(11)3659-8524">
+                    <BsTelephone />(11) 3659-8524
+                  </a>
+                </li>
+                <li>
+                  <a href="asiluminadas@gmail.com">
+                    asiluminadas@gmail.com
+                  </a>
+                </li>
+              </ul>
+            </div>
+            {/* Coluna 3 */}
+            <div className="col">
             <h5>Pagamentos</h5>
-              <FontAwesomeIcon icon={faBarcode} className="icons"/>
-              <FontAwesomeIcon icon={faPix} className="icons"/>
-              <FontAwesomeIcon icon={faCreditCard} className="icons"/>
-          </div>
-          {/* Coluna 4 */}
-          <div className="col">
-            <h5>Redes Socias</h5>
-            <ul className="list-unstyled">
-              <li>
-                <a href="https://www.instagram.com/atelieasiluminadas/">
-                  <BsInstagram /> Instagram
-                </a>
-              </li>
-              <li>
-                <a href="https://facebook.com">
-                  <FaFacebookSquare /> Facebook
-                </a>
-              </li>
-            </ul>
+              <div className="col-pag">
+                <FontAwesomeIcon icon={faBarcode} className="icons"/>
+                <FontAwesomeIcon icon={faPix} className="icons"/>
+                <FontAwesomeIcon icon={faCreditCard} className="icons"/>
+              </div>
+            </div>
+            {/* Coluna 4 */}
+            <div className="col">
+              <h5>Redes Socias</h5>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="https://www.instagram.com/atelieasiluminadas/">
+                    <BsInstagram /> Instagram
+                  </a>
+                </li>
+                <li>
+                  <a href="https://facebook.com">
+                    <FaFacebookSquare /> Facebook
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
