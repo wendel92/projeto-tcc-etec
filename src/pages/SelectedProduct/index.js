490b06c004a3fe4ae881ab38ffe2b185be52cbfc
ViniciusM11 <noturno793@gmail.com>
2022-05-29 17:26:25 -0300

    adicao de elementos e modif do css

--- src/pages/SelectedProduct/index.js
+++ src/pages/SelectedProduct/index.js
@@ -1,47 +1,84 @@
 import "./style.css";
-import vela from "./vela_decoupage.jpg"
-import React from "react";
+import vela from "./vela_decoupage.jpg";
+import React, { useEffect } from "react";
 import Header from "../../componentes/Header";
 import Footer from "../../componentes/Footer";
 import Card from "../../componentes/Card";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { Img } from "react-image";
+import { getAllProducts } from "../../services/products.service";
+import { getAllImages } from "../../services/images.services";
 
 export default function SelectedProduct() {
+  const [cards, setCards] = React.useState([]);
+  const [imagens, setImagens] = React.useState([]);
+
+  const consultarCards = async () => {
+    const response = await getAllProducts();
+    const lista = [];
+
+    response.map((card) => {
+      lista.push(card);
+    });
+    setCards(lista);
+  };
+
+  const consultarImages = async () => {
+    const response = await getAllImages();
+    const lista = [];
+
+    response.map((imagem) => {
+      lista.push(imagem);
+    });
+    setImagens(lista);
+  };
+
+  useEffect(() => {
+    consultarCards();
+    // consultarImagens()
+  }, []);
+
   return (
     <div className="main-container">
       <Header />
       <div className="cont1">
         <div className="cont2">
-          <div className="row">
-            <div className="col">
-              <div className="img">
-                <Img src={vela} className="img"></Img>
-              </div>    
-            </div>
-            <div className="col">
-                <br/>
-                <br/>   
-                <h2 className="mb mt">Vela Aromática - (5500)</h2>
-                <h3 className="mb">2 Disponíveis</h3>
-                <h3 className="mb">R$ 00,00</h3>
-                <br/>
+          <img src={vela} className="img-princ" sizes='(max-width: 799px) 100vw, (min-width: 800px) 800px'
+></img>
+          <div className="prod-info">
+            <div className="row-prod-info">
+              <div className="col-prod-info">
+                <h2>Vela Aromática - (5500)</h2>
+                <h3>2 Disponíveis</h3>
+                <h3>R$ 00,00</h3>
+                <br />
                 <Button className="btn">Comprar Agora</Button>
                 <Button className="btn">Adicionar ao Carrinho</Button>
+              </div>
             </div>
-            <h4>Descrição</h4>
-            <text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</text>
           </div>
-          <div className="row">
-            <div className="col">
-              <h4 className="txt-prodSem">Produtos Semelhantes</h4>
+          <div className="row-prod-detal">
+            <div className="col-prod-detal">
+              <h4>Descrição</h4>
+              <text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam.
+              </text>
             </div>
+          </div>
+          <h4 className="txt-prodSem">Produtos Semelhantes</h4>
+          <div className="row-prod-sem">
             <div className="home-page">
-              <Card/>
-              <Card/>
-              <Card/>
+              {cards &&
+                cards.map((item, index) => (
+                  <Card
+                    imagem={item.image}
+                    nome={item.name_product}
+                    preco={item.price_product}
+                    id={item.id}
+                  />
+                ))}
             </div>
           </div>
         </div>
